e27462ef127374b7f17f051d5e88644370416661
samayyy <samaykgada09@gmail.com>
2024-08-07 18:04:38 +0530

    changes

--- services/pinecone/PineconeService.js
+++ services/pinecone/PineconeService.js
@@ -36,19 +36,19 @@ const parameters = helpers.toValue({
 const safetySettings = [
   {
       "category": "HARM_CATEGORY_HARASSMENT",
-      "threshold": "BLOCK_NONE",
+      "threshold": "BLOCK_ONLY_HIGH",
   },
   {
       "category": "HARM_CATEGORY_HATE_SPEECH",
-      "threshold": "BLOCK_NONE",
+      "threshold": "BLOCK_ONLY_HIGH",
   },
   {
       "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-      "threshold": "BLOCK_NONE",
+      "threshold": "BLOCK_ONLY_HIGH",
   },
   {
       "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-      "threshold": "BLOCK_NONE",
+      "threshold": "BLOCK_ONLY_HIGH",
   },
 ]
 const { ChatVertexAI } = require("@langchain/google-vertexai");
