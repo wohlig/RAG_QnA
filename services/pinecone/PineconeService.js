f8ff409b6b054bcc1a8695081ec15a717c2b9181
Bilal2912 <bilalsani2912@gmail.com>
2024-08-19 16:38:10 +0530

    changes

--- services/pinecone/PineconeService.js
+++ services/pinecone/PineconeService.js
@@ -315,7 +315,47 @@ class PineconeService {
       throw error;
     }
   }
-  async askQna(question, prompt, sessionId) {
+  async makeDecisionFromGemini(question, chat) {
+    console.log("Rephrasing Question");
+    const model = new ChatVertexAI({
+      temperature: 0,
+      model: "gemini-1.5-pro",
+      safetySettings: safetySettings,
+    });
+    const structuredSchema = z.object({
+      answer: z.string().describe("'Yes' or 'No'"),
+      newQuestion: z
+        .string()
+        .describe(
+          "the new framed question based on the question asked and the chat history provided"
+        ),
+    });
+    const parser = StructuredOutputParser.fromZodSchema(structuredSchema);
+    const chain = RunnableSequence.from([
+      ChatPromptTemplate.fromTemplate(
+        `Analyze the given question and respond with "Yes" only if the user's question cannot be answered without referring to the chat history. If the chat history contains terms that are also found in the user's current question, respond with "No". Only when the user  mentions in their question something related to the previous conversation or indicates that the current question is connected to a previous topic, should you respond with "Yes" For all other cases, respond with "No". If 'Yes', analyze the chat history provided to determine the context. Then, rephrase the given question to include the necessary context from the chat history and assign the 'newQuestion' field with this new framed question and make sure the new question is short and to the point and only give the question, no extra information is required. If 'No', assign the 'newQuestion' field as empty string ('').
+    Question: ${question}
+    Chat History (List of all previous questions and their answers): ${JSON.stringify(
+      chat && chat.chatHistory && chat.chatHistory.length > 0
+        ? chat.chatHistory[chat.chatHistory.length - 1]
+        : []
+    )}
+        Format Instructions: {format_instructions}
+        `
+      ),
+      model,
+      parser,
+    ]);
+
+    const response = await chain.invoke({
+      question: question,
+      format_instructions: parser.getFormatInstructions(),
+    });
+
+    console.log(response);
+    return response;
+  }
+  async askQna(question, prompt, sessionId, chatId) {
     try {
       let finalQuestion = question;
       const versionLayer = await this.makeDecisionAboutVersionFromGemini(
