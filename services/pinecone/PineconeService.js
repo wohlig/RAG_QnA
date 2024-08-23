0f10995779754415044533327f0a48cf81eb38b7
Bilal2912 <bilalsani2912@gmail.com>
2024-08-23 10:24:40 +0530

    changes

--- services/pinecone/PineconeService.js
+++ services/pinecone/PineconeService.js
@@ -391,9 +391,9 @@ class PineconeService {
                       base.source AS source
                       FROM
                       VECTOR_SEARCH(
-                        TABLE ondc_dataset.ondc_gemini,
+                        TABLE ondc_dataset.ondc_gemini_copy,
                         'embedding',
-                          (SELECT ${embeddingString} AS embedding FROM ondc_dataset.ondc_gemini),
+                          (SELECT ${embeddingString} AS embedding FROM ondc_dataset.ondc_gemini_copy),
                         top_k => 20,
                         distance_type => 'COSINE'
                       ) `;
